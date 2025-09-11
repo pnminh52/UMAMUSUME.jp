d652451640270d724132e220113c6eadf9501646
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2025-09-11 08:28:12 +0700

    fix

--- src/pages/contents/TopContents.jsx
+++ src/pages/contents/TopContents.jsx
@@ -1,21 +1,18 @@
-import React from 'react'
-import Title from './../../components/news/Title';
-import Laptop from './../../components/contents/topContents/Laptop';
-import Mobile from './../../components/contents/topContents/Mobile';
+import React from "react";
+import Title from "./../../components/news/Title";
+import Laptop from "./../../components/contents/topContents/Laptop";
+import Mobile from "./../../components/contents/topContents/Mobile";
 
 const TopContents = () => {
-
- 
   return (
     <div className="bg-gray-100  space-y-8 py-8">
-        <Title />
-<Laptop />
-<Mobile />
-
-
-      
+      <Title />
+      <Laptop />
+      <div className="block sm:hidden">
+        <Mobile />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TopContents
+export default TopContents;
